0e46a3eb4c22fb9d66f4da4de859c82521a1c75b
Jaikaransingh <jaisingh6653@gmail.com>
2023-05-25 17:23:39 +0530

    Author done

--- src/routes/route.js
+++ src/routes/route.js
@@ -10,6 +10,9 @@ const { login } = require('../controllers/authorController.js')
 // login author
 router.post('/login', login)
 
+//Api for authorController to 
+router.post("/authors", authorController.createAuthor)
+router.get("/authors", authorController.getAuthor)
 
 
 module.exports = router
