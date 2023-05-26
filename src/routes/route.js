46fd5584deb450753ef28717de4688082ad08be9
Priyanshsharma21 <piyuindia4@gmail.com>
2023-05-26 14:12:09 +0530

    complete with auth

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,14 +2,15 @@ const express = require('express')
 const router = express.Router()
 const { login,createAuthor,getAuthor } = require('../controllers/authorController.js')
 const { deleteBlogById,deleteByQuerying,createBlog,getBlog,getAllBlogs,updateBlog } = require('../controllers/blogController.js')
+const { isLoggedIn } = require('../middlewares/index.js')
 
 
 
 //blog routes
-router.post('/createBlog' , createBlog)
+router.post('/createBlog',isLoggedIn, createBlog)
 router.get('/getBlog' , getBlog);
 router.get('/blogs' , getAllBlogs);
-router.put('/blogs/:blogId' , updateBlog);
+router.put('/blogs/:blogId' ,isLoggedIn, updateBlog);
 
 
 
@@ -22,8 +23,8 @@ router.get("/authors", getAuthor)
 router.post("/authors", createAuthor)
 
 
-router.delete('/blogs?queryParams',deleteByQuerying)
-router.delete('/blogs/:blogId',deleteBlogById)
+router.delete('/blogs?queryParams',isLoggedIn,deleteByQuerying)
+router.delete('/blogs/:blogId',isLoggedIn,deleteBlogById)
 
 
 
