efde4c5dfd7f28e79f9d67292a812f5c41602f75
kashi <kashisharmaujjn@gmail.com>
2023-05-27 10:51:29 +0530

    phase-2

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 const app = require('./app.js')
 const mongoose = require('mongoose')
-const PORT = 8080
 
 require('dotenv').config();
 
-// const { PORT, MONGODB_URL } = process.env
+const { PORT, MONGODB_URL } = process.env
 
-// console.log(MONGODB_URL)
+
 const startServer = async()=>{
     try {
         mongoose.set('strictQuery', true)
-        await mongoose.connect("mongodb+srv://khjp1:3BSJy3TDTOJYRLmd@blog.b9eele7.mongodb.net/group2Database"        ,{
+        await mongoose.connect(MONGODB_URL,{
             useNewUrlParser : true,
             useUnifiedTopology : true
         })
